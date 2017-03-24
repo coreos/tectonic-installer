bdb80ad616c8a034d873de3405cc76b592a06506
Alex Somesan <alex.somesan@gmail.com>
2017-03-24 10:41:45 +0100

    Fixes for BYO VPC: * don't condition the creation of the VPC resource   (https://github.com/hashicorp/hil/issues/50) * pass external subnet IDs into the VPC module

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -1,14 +1,21 @@
 data "aws_availability_zones" "azs" {}
 
 resource "aws_vpc" "new_vpc" {
-  count                = "${length(var.external_vpc_id) > 0 ? 0 : 1}"
-  cidr_block           = "${var.cidr_block}"
+  # count                = "${length(var.external_vpc_id) > 0 ? 0 : 1}"
+  #
+  # We can't yet use the count gate here because of terraform issues:
+  # https://github.com/hashicorp/hil/issues/50
+  # https://github.com/hashicorp/terraform/issues/11566
+  # This should be re-enabled when above issues are fixed.
+  #
+  cidr_block = "${var.cidr_block}"
+
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags {
-    Name              = "${var.cluster_name}"
-    KubernetesCluster = "${var.cluster_name}"
+    Name              = "${var.external_vpc_id == "" ? var.cluster_name : "${var.cluster_name}-side-effect"}"
+    KubernetesCluster = "${var.external_vpc_id == "" ? var.cluster_name : "${var.cluster_name}-side-effect"}"
   }
 }
 
