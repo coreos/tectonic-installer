6d127a97f765399dd187be9471e05d73a4fdd2ab
Alex Somesan <alex.somesan@gmail.com>
2017-03-24 18:10:03 +0100

    Diabolical use of join() to circumvent conditional operator limitation on missing resources.

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -1,13 +1,8 @@
 data "aws_availability_zones" "azs" {}
 
 resource "aws_vpc" "new_vpc" {
-  # count                = "${length(var.external_vpc_id) > 0 ? 0 : 1}"
-  #
-  # We can't yet use the count gate here because of terraform issues:
-  # https://github.com/hashicorp/hil/issues/50
-  # https://github.com/hashicorp/terraform/issues/11566
-  # This should be re-enabled when above issues are fixed.
-  #
+  count = "${var.external_vpc_id == "" ? 0 : 1}"
+
   cidr_block = "${var.cidr_block}"
 
   enable_dns_hostnames = true
@@ -20,5 +15,5 @@ resource "aws_vpc" "new_vpc" {
 }
 
 data "aws_vpc" "cluster_vpc" {
-  id = "${var.external_vpc_id == "" ? aws_vpc.new_vpc.id : var.external_vpc_id }"
+  id = "${var.external_vpc_id == "" ? join(" ", aws_vpc.new_vpc.*.id) : var.external_vpc_id }"
 }
