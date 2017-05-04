73cd42813ded25efcb77f1812bff8e5fceafbafe
Dan Gillespie <dan.gillespie@coreos.com>
2017-05-03 20:35:30 -0700

    added

--- installer/cmd/clusterctl/cluster/cluster.go
+++ installer/cmd/clusterctl/cluster/cluster.go
@@ -2,13 +2,16 @@ package cluster
 
 // Cluster represents a Tectonic instance.
 type Cluster struct {
+	// Config describes the parameters the cluster is to be provisioned with.
+	*Config
+
 	// ActiveScenarios are the scenarios that are currently being applied to the cluster
 	ActiveScenarios []string
 }
 
-// CanApply validates whether scenario s with name n can be applied to this cluster. A scenario may not be applied
+// Tolerable validates whether scenario s with name n can be applied to this cluster. A scenario may not be applied
 // because another variant from the scenario has already been applied or there's a constraint with an existing scenario.
-func (c Cluster) CanApply(s Scenario, name string) bool {
+func (c Cluster) Tolerable(s Scenario, name string) bool {
 	// should not be applied with avoided scenarios, or it's own scenario
 	rejects := append(s.Avoid, name)
 
@@ -22,6 +25,9 @@ func (c Cluster) CanApply(s Scenario, name string) bool {
 	return true
 }
 
-// Clusters represent a grouping of clusters.
-type Clusters []Config
+// Add applies the config to the cluster and records the scenarios name. Restrictions are not checked.
+func (c *Cluster) Add(cfg Config, scenarioName string) {
+	c.ActiveScenarios = append(c.ActiveScenarios, scenarioName)
+	c.Config.Apply(cfg)
+}
 
--- installer/cmd/clusterctl/cluster/config.go
+++ installer/cmd/clusterctl/cluster/config.go
@@ -9,12 +9,9 @@ type Config struct {
 	server.TerraformApplyHandlerInput
 }
 
-// Apply overrides a cluster's configuration using values from another cluster. Currently only works on variables.
+// Apply overrides configuration using values from another config. Currently only works on variables.
 func (c *Config) Apply(top Config) {
 	for k, v := range top.Variables {
 		c.Variables[k] = v
 	}
-
-	// combine recording of applied scenarios
-	c.ActiveScenarios = append(c.ActiveScenarios, top.ActiveScenarios...)
 }
