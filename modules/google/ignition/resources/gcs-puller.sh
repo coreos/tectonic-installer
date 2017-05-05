fc035c3923813da9fbf671ca18638aca9282629c
Eric Johnson <erjohnso@google.com>
2017-05-05 16:23:07 +0000

    wip: gcs-puller script working

--- modules/google/ignition/resources/gcs-puller.sh
+++ modules/google/ignition/resources/gcs-puller.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -x
 
 if [ "$#" -ne "2" ]; then
     echo "Usage: $0 location destination"
     exit 1
 fi
 
-alias gsutil='(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net=host -v /tmp:/gs google/cloud-sdk gsutil'
+docker pull google/cloud-sdk > /dev/null
+gsutil="docker run -t --net=host -v /tmp:/gs google/cloud-sdk gsutil"
+assets=$(basename ${1})
 
-# CoreOS will automatically pull the 'gcloud-sdk' docker image. If the GCE
-# VMs are set up to allow storage.objectReader scope or better, then gsutil
-# will be able to copy assets from GCS.
-gsutil cp gs://${1} /gs/${1}
-/usr/bin/sudo mv /tmp/${1} $2
+${gsutil} cp gs://${1} /gs/${assets}
+/usr/bin/sudo mv /tmp/${assets} ${2}
--- platforms/google/main.tf
+++ platforms/google/main.tf
@@ -135,8 +135,8 @@ module "ignition-masters" {
   kubelet_node_taints       = "node-role.kubernetes.io/master=:NoSchedule"
   kube_dns_service_ip       = "${var.tectonic_kube_dns_service_ip}"
   etcd_endpoints            = ["${module.etcd.etcd_ip}"]
-  kubeconfig_gcs_location   = "${google_storage_bucket.kubeconfig.name}/${google_storage_bucket_object.kubeconfig.name}"
-  assets_gcs_location       = "${google_storage_bucket.tectonic-assets.name}/${google_storage_bucket_object.tectonic-assets.name}"
+  kubeconfig_gcs_location   = "${google_storage_bucket.tectonic.name}/${google_storage_bucket_object.kubeconfig.name}"
+  assets_gcs_location       = "${google_storage_bucket.tectonic.name}/${google_storage_bucket_object.tectonic-assets.name}"
   container_images          = "${var.tectonic_container_images}"
   bootkube_service          = "${module.bootkube.systemd_service}"
   tectonic_service          = "${module.tectonic.systemd_service}"
@@ -151,7 +151,7 @@ module "ignition-workers" {
   kubelet_node_taints     = ""
   kube_dns_service_ip     = "${var.tectonic_kube_dns_service_ip}"
   etcd_endpoints          = ["${module.etcd.etcd_ip}"]
-  kubeconfig_gcs_location = "${google_storage_bucket.kubeconfig.name}/${google_storage_bucket_object.kubeconfig.name}"
+  kubeconfig_gcs_location = "${google_storage_bucket.tectonic.name}/${google_storage_bucket_object.kubeconfig.name}"
   assets_gcs_location     = ""
   container_images        = "${var.tectonic_container_images}"
   bootkube_service        = ""
