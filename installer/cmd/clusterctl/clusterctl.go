82cfd5f1c7a99f51c37c96dc531afa1eecbb61fe
Dan Gillespie <dan.gillespie@coreos.com>
2017-05-11 02:10:29 -0700

    installer/cmd/clusterctl: added cli

--- installer/cmd/clusterctl/clusterctl.go
+++ installer/cmd/clusterctl/clusterctl.go
@@ -1,1 +1,49 @@
-package clusterctl
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+/*
+	clusterctl [-s] <file>
+
+	clusterctl deploys clusters using the Tectonic installer executable.
+
+	Arguments
+	- Files describing clusters to be deployed or specs to be generated into clusters.
+
+	-g .
+*/
+
+var (
+	flags = struct {
+		generate bool
+	}{}
+
+	// file to be parsed
+	files []string
+)
+
+func init() {
+	flag.BoolVar(&flags.generate, "g", false, "output a set of clusters generated using a spec")
+}
+
+func main() {
+	file := flag.Arg(0)
+	if len(file) < 1 {
+		fmt.Println("a file must be specified")
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("could not read file: ")
+	}
+
+	if flags.generate {
+		return generate(data)
+	}
+}
