openshift/installer

commit 904688afc5f66d400ad38dbbd7e201103fea40cc

Tectonic Monitoring 1.2 (#771)
* monitoring: update manifests to match tectonic-monitoring 1.2.0

* monitoring: remove unused configmap

modules/tectonic/resources/tectonic.sh
@@ -114,23 +114,33 @@ kubectl create -f console/service.yaml
 kubectl create -f console/deployment.yaml
 
 echo "Creating Tectonic Monitoring"
-kubectl create -f monitoring/prometheus-operator-service-account.yaml
-kubectl create -f monitoring/prometheus-operator-cluster-role.yaml
 kubectl create -f monitoring/prometheus-operator-cluster-role-binding.yaml
-kubectl create -f monitoring/prometheus-k8s-service-account.yaml
-kubectl create -f monitoring/prometheus-k8s-cluster-role.yaml
-kubectl create -f monitoring/prometheus-k8s-cluster-role-binding.yaml
-kubectl create -f monitoring/prometheus-k8s-config.yaml
-kubectl create -f monitoring/prometheus-k8s-rules.yaml
-kubectl create -f monitoring/prometheus-svc.yaml
-kubectl create -f monitoring/node-exporter-svc.yaml
-kubectl create -f monitoring/node-exporter-ds.yaml
+kubectl create -f monitoring/prometheus-operator-cluster-role.yaml
+kubectl create -f monitoring/prometheus-operator-service-account.yaml
 kubectl create -f monitoring/prometheus-operator.yaml
+
 wait_for_tpr tectonic-system prometheus.monitoring.coreos.com
 wait_for_tpr tectonic-system service-monitor.monitoring.coreos.com
-kubectl create -f monitoring/prometheus-k8s.yaml
-kubectl create -f monitoring/prometheus-k8s-service-monitor-k8s-apps-http.yaml
+
+kubectl create -f monitoring/kube-state-metrics-cluster-role-binding.yaml
+kubectl create -f monitoring/kube-state-metrics-cluster-role.yaml
+kubectl create -f monitoring/kube-state-metrics-deployment.yaml
+kubectl create -f monitoring/kube-state-metrics-service-account.yaml
+kubectl create -f monitoring/kube-state-metrics-service.yaml
+kubectl create -f monitoring/node-exporter-ds.yaml
+kubectl create -f monitoring/node-exporter-svc.yaml
+kubectl create -f monitoring/prometheus-k8s-cluster-role-binding.yaml
+kubectl create -f monitoring/prometheus-k8s-cluster-role.yaml
+kubectl create -f monitoring/prometheus-k8s-config.yaml
+kubectl create -f monitoring/prometheus-k8s-rules.yaml
+kubectl create -f monitoring/prometheus-k8s-service-account.yaml
+kubectl create -f monitoring/prometheus-k8s-service-monitor-apiserver.yaml
+kubectl create -f monitoring/prometheus-k8s-service-monitor-kube-state-metrics.yaml
 kubectl create -f monitoring/prometheus-k8s-service-monitor-kubelet.yaml
+kubectl create -f monitoring/prometheus-k8s-service-monitor-node-exporter.yaml
+kubectl create -f monitoring/prometheus-k8s-service-monitor-prometheus.yaml
+kubectl create -f monitoring/prometheus-k8s.yaml
+kubectl create -f monitoring/prometheus-svc.yaml
 
 echo "Creating Ingress"
 kubectl create -f ingress/default-backend/configmap.yaml

config.tf
@@ -25,9 +25,10 @@ variable "tectonic_container_images" {
     kube_version_operator           = "quay.io/coreos/kube-version-operator:v1.6.2"
     tectonic_channel_operator       = "quay.io/coreos/tectonic-channel-operator:0.3.2"
     node_agent                      = "quay.io/coreos/node-agent:787844277099e8c10d617c3c807244fc9f873e46"
-    prometheus_operator             = "quay.io/coreos/prometheus-operator:v0.8.2"
-    tectonic_prometheus_operator    = "quay.io/coreos/tectonic-prometheus-operator:v1.1.0"
-    node_exporter                   = "quay.io/prometheus/node-exporter:v0.13.0"
+    prometheus_operator             = "quay.io/coreos/prometheus-operator:v0.9.1"
+    tectonic_prometheus_operator    = "quay.io/coreos/tectonic-prometheus-operator:v1.2.0"
+    node_exporter                   = "quay.io/prometheus/node-exporter:v0.14.0"
+    kube_state_metrics              = "quay.io/coreos/kube-state-metrics:v0.5.0"
     config_reload                   = "quay.io/coreos/configmap-reload:v0.0.1"
     heapster                        = "gcr.io/google_containers/heapster:v1.3.0"
     addon_resizer                   = "gcr.io/google_containers/addon-resizer:1.7"
@@ -53,8 +54,8 @@ variable "tectonic_versions" {
 
   default = {
     etcd       = "3.1.6"
-    prometheus = "v1.6.1"
-    monitoring = "1.1.0"
+    prometheus = "v1.6.3"
+    monitoring = "1.2.0"
     kubernetes = "1.6.2+tectonic.1"
     tectonic   = "1.6.2-tectonic.1"
   }

modules/tectonic/assets.tf
@@ -20,6 +20,7 @@ resource "template_dir" "tectonic" {
     kube_version_operator_image           = "${var.container_images["kube_version_operator"]}"
     node_agent_image                      = "${var.container_images["node_agent"]}"
     node_exporter_image                   = "${var.container_images["node_exporter"]}"
+    kube_state_metrics_image              = "${var.container_images["kube_state_metrics"]}"
     prometheus_operator_image             = "${var.container_images["prometheus_operator"]}"
     stats_emitter_image                   = "${var.container_images["stats_emitter"]}"
     stats_extender_image                  = "${var.container_images["stats_extender"]}"

modules/tectonic/resources/manifests/monitoring/kube-state-metrics-cluster-role-binding.yaml
@@ -1,0 +1,12 @@
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: kube-state-metrics
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: kube-state-metrics
+subjects:
+- kind: ServiceAccount
+  name: kube-state-metrics
+  namespace: tectonic-system

modules/tectonic/resources/manifests/monitoring/kube-state-metrics-cluster-role.yaml
@@ -1,0 +1,21 @@
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRole
+metadata:
+  name: kube-state-metrics
+rules:
+- apiGroups: [""]
+  resources:
+  - nodes
+  - pods
+  - services
+  - resourcequotas
+  - replicationcontrollers
+  - limitranges
+  verbs: ["list", "watch"]
+- apiGroups: ["extensions"]
+  resources:
+  - daemonsets
+  - deployments
+  - replicasets
+  verbs: ["list", "watch"]
+

modules/tectonic/resources/manifests/monitoring/kube-state-metrics-deployment.yaml
@@ -1,0 +1,29 @@
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: kube-state-metrics
+  namespace: tectonic-system
+  labels:
+    k8s-app: kube-state-metrics
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        k8s-app: kube-state-metrics
+    spec:
+      serviceAccountName: kube-state-metrics
+      containers:
+      - name: kube-state-metrics
+        image: ${kube_state_metrics_image}
+        ports:
+        - name: metrics
+          containerPort: 8080
+        resources:
+          requests:
+            memory: 100Mi
+            cpu: 100m
+          limits:
+            memory: 200Mi
+            cpu: 200m
+

modules/tectonic/resources/manifests/monitoring/kube-state-metrics-service-account.yaml
@@ -1,0 +1,5 @@
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: kube-state-metrics
+  namespace: tectonic-system

modules/tectonic/resources/manifests/monitoring/kube-state-metrics-service.yaml
@@ -1,16 +1,17 @@
 apiVersion: v1
 kind: Service
 metadata:
-  name: node-exporter
+  name: kube-state-metrics
   namespace: tectonic-system
   labels:
-    k8s-app: node-exporter
+    k8s-app: kube-state-metrics
 spec:
   type: ClusterIP
-  clusterIP: None
   ports:
   - name: http-metrics
-    port: 9100
+    port: 8080
+    targetPort: metrics
     protocol: TCP
   selector:
-    k8s-app: node-exporter
+    k8s-app: kube-state-metrics
+

coreos/tectonic-installer

commit f8502da8ee5c1e3d5e1ce9773ccc1cbae8ebe32a

modules/tectonic: remove prometheus-k8s-confi.yaml from tectonic.sh
Regression introduced in #771

modules/tectonic/resources/tectonic.sh
@@ -131,7 +131,6 @@ kubectl create -f monitoring/node-exporter-ds.yaml
 kubectl create -f monitoring/node-exporter-svc.yaml
 kubectl create -f monitoring/prometheus-k8s-cluster-role-binding.yaml
 kubectl create -f monitoring/prometheus-k8s-cluster-role.yaml
-kubectl create -f monitoring/prometheus-k8s-config.yaml
 kubectl create -f monitoring/prometheus-k8s-rules.yaml
 kubectl create -f monitoring/prometheus-k8s-service-account.yaml
 kubectl create -f monitoring/prometheus-k8s-service-monitor-apiserver.yaml