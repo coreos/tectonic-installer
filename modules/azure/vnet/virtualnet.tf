3a7f61617ca73ad6bb2750a4baa9cf81899a59df
Stephen Augustus <augustus@coreos.com>
2017-05-25 20:19:51 -0700

    azure/net: Map new NSGs to master and worker subnet

--- modules/azure/vnet/virtualnet.tf
+++ modules/azure/vnet/virtualnet.tf
@@ -12,6 +12,7 @@ resource "azurerm_subnet" "master_subnet" {
   resource_group_name  = "${var.resource_group_name}"
   virtual_network_name = "${var.external_vnet_name == "" ? join("",azurerm_virtual_network.tectonic_vnet.*.name) : var.external_vnet_name }"
   address_prefix       = "${cidrsubnet(var.vnet_cidr_block, 4, 0)}"
+  network_security_group_id = "${azurerm_network_security_group.master.id}"
 }
 
 resource "azurerm_subnet" "worker_subnet" {
@@ -20,8 +21,11 @@ resource "azurerm_subnet" "worker_subnet" {
   resource_group_name  = "${var.resource_group_name}"
   virtual_network_name = "${var.external_vnet_name == "" ? join("",azurerm_virtual_network.tectonic_vnet.*.name) : var.external_vnet_name }"
   address_prefix       = "${cidrsubnet(var.vnet_cidr_block, 4, 1)}"
+  network_security_group_id = "${azurerm_network_security_group.worker.id}"
 }
 
+# TODO: Add NSG ID
+# TODO: Review if we actually need an additional subnet for this
 resource "azurerm_subnet" "admin_subnet" {
   count                = "${var.external_vnet_name == "" ? 1 : 0}"
   name                 = "${var.tectonic_cluster_name}_admin_subnet"
