25074fb62e6dc97f7e1765b2cc8ad89c453d11f4
Aleks Saul <aleks.saul@gmail.com>
2017-05-31 16:34:26 -0400

    vmware: run terraform fmt

--- platforms/vmware/tectonic.tf
+++ platforms/vmware/tectonic.tf
@@ -26,11 +26,11 @@ module "bootkube" {
   oidc_groups_claim   = "groups"
   oidc_client_id      = "tectonic-kubectl"
 
-  etcd_endpoints       = ["${module.etcd.endpoints}"]
-  etcd_ca_cert         = "${var.tectonic_etcd_ca_cert_path}"
-  etcd_client_cert     = "${var.tectonic_etcd_client_cert_path}"
-  etcd_client_key      = "${var.tectonic_etcd_client_key_path}"
-  
+  etcd_endpoints   = ["${module.etcd.endpoints}"]
+  etcd_ca_cert     = "${var.tectonic_etcd_ca_cert_path}"
+  etcd_client_cert = "${var.tectonic_etcd_client_cert_path}"
+  etcd_client_key  = "${var.tectonic_etcd_client_key_path}"
+
   experimental_enabled = "${var.tectonic_experimental}"
 }
 
