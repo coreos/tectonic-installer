439da723ec29e1a203aa482e188eb224a0a7ed05
Colin Hom <colhom@gmail.com>
2017-06-05 13:33:49 -0700

    modules/aws: parametrize pause container image

--- modules/aws/ignition/ignition.tf
+++ modules/aws/ignition/ignition.tf
@@ -42,6 +42,7 @@ data "template_file" "kubelet" {
     node_label             = "${var.kubelet_node_label}"
     node_taints_param      = "${var.kubelet_node_taints != "" ? "--register-with-taints=${var.kubelet_node_taints}" : ""}"
     kubeconfig_s3_location = "${var.kubeconfig_s3_location}"
+    pod_infra_image        = "${var.container_images["pod_infra_image"]}"
   }
 }
 
