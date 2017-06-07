f08d8da0f4e983cb4f64bdfa940f50eb36838035
Stephen Augustus <augustus@coreos.com>
2017-06-07 06:22:20 -0700

    azure/nsg: Fix NSG rule priorities

--- modules/azure/vnet/nsg-lb.tf
+++ modules/azure/vnet/nsg-lb.tf
@@ -23,7 +23,7 @@ resource "azurerm_network_security_group" "api" {
 resource "azurerm_network_security_rule" "api_egress" {
   count                       = "${var.external_nsg_api == "" ? 1 : 0}"
   name                        = "${var.tectonic_cluster_name}-api_egress"
-  priority                    = 100
+  priority                    = 1990
   direction                   = "Outbound"
   access                      = "Allow"
   protocol                    = "*"
@@ -38,7 +38,7 @@ resource "azurerm_network_security_rule" "api_egress" {
 resource "azurerm_network_security_rule" "api_ingress_https" {
   count                       = "${var.external_nsg_api == "" ? 1 : 0}"
   name                        = "${var.tectonic_cluster_name}-api_ingress_https"
-  priority                    = 200
+  priority                    = 300
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "tcp"
@@ -60,7 +60,7 @@ resource "azurerm_network_security_group" "console" {
 resource "azurerm_network_security_rule" "console_egress" {
   count                       = "${var.external_nsg_api == "" ? 1 : 0}"
   name                        = "${var.tectonic_cluster_name}-console_egress"
-  priority                    = 100
+  priority                    = 1995
   direction                   = "Outbound"
   access                      = "Allow"
   protocol                    = "*"
@@ -75,7 +75,7 @@ resource "azurerm_network_security_rule" "console_egress" {
 resource "azurerm_network_security_rule" "console_ingress_https" {
   count                       = "${var.external_nsg_api == "" ? 1 : 0}"
   name                        = "${var.tectonic_cluster_name}-console_ingress_https"
-  priority                    = 200
+  priority                    = 305
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "tcp"
@@ -90,7 +90,7 @@ resource "azurerm_network_security_rule" "console_ingress_https" {
 resource "azurerm_network_security_rule" "console_ingress_http" {
   count                       = "${var.external_nsg_api == "" ? 1 : 0}"
   name                        = "${var.tectonic_cluster_name}-console_ingress_http"
-  priority                    = 300
+  priority                    = 310
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "tcp"
