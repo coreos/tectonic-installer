d020b45b205e5c22a9a990b60a8678f593786560
Sergiusz Urbaniak <sur@coreos.com>
2017-06-20 16:40:50 +0200

    modules/bootkube: enable TLS for self-hosted etcd
    Fixes #1113

--- config.tf
+++ config.tf
@@ -148,7 +148,7 @@ EOF
 
 variable "tectonic_etcd_client_cert_path" {
   type    = "string"
-  default = ""
+  default = "/dev/null"
 
   description = <<EOF
 (optional) The path of the file containing the client certificate for TLS communication with etcd.
@@ -160,7 +160,7 @@ EOF
 
 variable "tectonic_etcd_client_key_path" {
   type    = "string"
-  default = ""
+  default = "/dev/null"
 
   description = <<EOF
 (optional) The path of the file containing the client key for TLS communication with etcd.
