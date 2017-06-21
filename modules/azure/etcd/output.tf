d9213a9b7761b045ffb0bd6938348d8480077d09
Alex Somesan <alex.somesan@gmail.com>
2017-06-21 15:10:03 +0200

    modules/azure: remove LB and public addresses from etcd

--- modules/azure/etcd/output.tf
+++ modules/azure/etcd/output.tf
@@ -1,3 +1,1 @@
-output "endpoints" {
-  value = ["${split(",", length(var.external_endpoints) == 0 ? join(",", azurerm_network_interface.etcd_nic.*.private_ip_address) : join(",", var.external_endpoints))}"]
-}
+
