5075f7ff5b53003d38edd066a80e3539841c9d2d
Philipp Strube <pst@coreos.com>
2017-06-21 21:10:37 +0200

    Add required TLS variables after rebase

--- platforms/google/tectonic.tf
+++ platforms/google/tectonic.tf
@@ -39,11 +39,24 @@ module "bootkube" {
   oidc_groups_claim   = "groups"
   oidc_client_id      = "tectonic-kubectl"
 
-  etcd_endpoints       = ["${module.etcd.etcd_ip}"]
-  etcd_ca_cert         = "${var.tectonic_etcd_ca_cert_path}"
-  etcd_client_cert     = "${var.tectonic_etcd_client_cert_path}"
-  etcd_client_key      = "${var.tectonic_etcd_client_key_path}"
+  etcd_endpoints   = ["${module.etcd.etcd_ip}"]
+  etcd_ca_cert     = "${var.tectonic_etcd_ca_cert_path}"
+  etcd_client_cert = "${var.tectonic_etcd_client_cert_path}"
+  etcd_client_key  = "${var.tectonic_etcd_client_key_path}"
+
+  etcd_cert_dns_names = [
+    "${var.tectonic_cluster_name}-etcd-0.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-1.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-2.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-3.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-4.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-5.${var.tectonic_base_domain}",
+    "${var.tectonic_cluster_name}-etcd-6.${var.tectonic_base_domain}",
+  ]
+
   experimental_enabled = "${var.tectonic_experimental}"
+
+  master_count = "${var.tectonic_master_count}"
 }
 
 module "tectonic" {
