0cf5ea6f360b2924e45a42895f36603deff05da3
Max Leonard Inden <IndenML@gmail.com>
2017-06-27 17:38:24 +0200

    Use merge list for container images in `modules/update-payload/assets.tf`

--- modules/update-payload/assets.tf
+++ modules/update-payload/assets.tf
@@ -18,15 +18,15 @@ resource "template_dir" "upload_payload" {
     alertmanager_image             = ""
 
     # Actual variables we care about when generating the payload.
-    container_linux_update_operator_image = "${var.tectonic_container_images["container_linux_update_operator"]}"
-    kube_version_operator_image           = "${var.tectonic_container_images["kube_version_operator"]}"
-    tectonic_channel_operator_image       = "${var.tectonic_container_images["tectonic_channel_operator"]}"
-    tectonic_prometheus_operator_image    = "${var.tectonic_container_images["tectonic_prometheus_operator"]}"
-    tectonic_etcd_operator_image          = "${var.tectonic_container_images["tectonic_etcd_operator"]}"
+    container_linux_update_operator_image = "${lookup(merge(var.tectonic_container_images, var.tectonic_user_container_images), "container_linux_update_operator")}"
+    kube_version_operator_image           = "${lookup(merge(var.tectonic_container_images, var.tectonic_user_container_images), "kube_version_operator")}"
+    tectonic_channel_operator_image       = "${lookup(merge(var.tectonic_container_images, var.tectonic_user_container_images), "tectonic_channel_operator")}"
+    tectonic_prometheus_operator_image    = "${lookup(merge(var.tectonic_container_images, var.tectonic_user_container_images), "tectonic_prometheus_operator")}"
+    tectonic_etcd_operator_image          = "${lookup(merge(var.tectonic_container_images, var.tectonic_user_container_images), "tectonic_etcd_operator")}"
 
     kubernetes_version             = "${var.tectonic_versions["kubernetes"]}"
     monitoring_version             = "${var.tectonic_versions["monitoring"]}"
     tectonic_version               = "${var.tectonic_versions["tectonic"]}"
-    tectonic_etcd_operator_version = "${element(split(":", var.tectonic_container_images["tectonic_etcd_operator"]), 1)}"
+    tectonic_etcd_operator_version = "${element(split(":", lookup(merge(var.tectonic_container_images, var.tectonic_user_container_images),"tectonic_etcd_operator")), 1)}"
   }
 }
