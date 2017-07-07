0ca8bb514bc294b33e44a7a46885c82744953715
Max Leonard Inden <IndenML@gmail.com>
2017-07-07 15:12:45 +0200

    Azure tests: Fix shellcheck

--- tests/smoke/azure/smoke.sh
+++ tests/smoke/azure/smoke.sh
@@ -50,7 +50,7 @@ common() {
     echo "selected region: $TF_VAR_tectonic_azure_location"
     echo "cluster name: $CLUSTER"
 
-    echo $AZURE_SMOKE_SSH_KEY_PUB > ~/.ssh/id_rsa_azure.pub
+    echo "$AZURE_SMOKE_SSH_KEY_PUB" > ~/.ssh/id_rsa_azure.pub
     export TF_VAR_tectonic_azure_ssh_key
     TF_VAR_tectonic_azure_ssh_key=$(realpath ~/.ssh/id_rsa_azure.pub)
 
