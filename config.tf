openshift/installer

commit 54be4a9225587f6867b6c391831db4d90a54b79c

Azure cleanup (#1154)
* modules/azure/vnet: remove etcd load-balancer

* modules/azure/etcd: etcd module outputs explicit node IPs

* modules/azure: remove LB and public addresses from etcd

* */azure: make DNS optional and allow for externally supplied zone

* platforms/azure: remove unused DNS records for workers

* */azure: refactor networking to vnet module. configurable CL version

* */azure: improve resource names, remove boot diags

* */azure/etcd: add TLS support for self-provisioned etcd

* */azure: fix usage of cidr variables

* */azure: advertise etcd nodes via DNS names

* modules/azure: turn off etcd related resources when running self-hosted etcd

* modules/azure: skip creating etcd sec-groups when self-hosted

* */azure: DNS clean-up

* make DNS provisioning optional
* DNS records not used as inputs into other modules
* create records for etcd nodes (useful in private environments)
* etcd cluster discovery only uses Azure DNS

* */azure: Networking clean-up

* only accept resource IDs (not names) as inputs for external VNet, Subnets, etc.
* parse the resource-groups and resource names out of input IDs
* allow suppling multiple combinations of external networking resources

* modules/azure: use '-api' suffix for API FQDNs

* platforms/azure: review configuration variables and descriptions

* platforms/azure: set default etcd_count values

config.tf has tectonic_etcd_count set to 0 (zero) by default. The
meaning of that value is "autodetect" on other platforms.

I.e. on AWS we deploy three etcd nodes by default or spread it across
availability zones.

At Azure we currently set 1 (one) for simplicity.

* examples/azure: refresh example config

* */azure: propagate 'tectonic_cl_channel' value into VM resources

* platforms/azure: don't wrap arrays in arrays

In terraform, this syntax has changed meaning with the new typed vars.

* modules/azure/etcd: compact node_names output

config.tf
@@ -68,13 +68,14 @@ variable "tectonic_versions" {
   type        = "map"
 
   default = {
-    etcd          = "3.1.8"
-    prometheus    = "v1.7.1"
-    alertmanager  = "v0.7.1"
-    monitoring    = "1.3.0"
-    kubernetes    = "1.6.7+tectonic.1"
-    tectonic      = "1.6.7-tectonic.1"
-    tectonic-etcd = "0.0.1"
+    container_linux = "1353.8.0"
+    etcd            = "3.1.8"
+    prometheus      = "v1.7.1"
+    alertmanager    = "v0.7.1"
+    monitoring      = "1.3.0"
+    kubernetes      = "1.6.6+tectonic.1"
+    tectonic        = "1.6.6-tectonic.1"
+    tectonic-etcd   = "0.0.1"
   }
 }
 
@@ -330,4 +331,48 @@ variable "tectonic_stats_url" {
   type        = "string"
   default     = "https://stats-collector.tectonic.com"
   description = "The Tectonic statistics collection URL to which to report."
+}
+
+variable "tectonic_ddns_server" {
+  type    = "string"
+  default = ""
+
+  description = <<EOF
+(optional) This only applies if you use the modules/dns/ddns module.
+
+Specifies the RFC2136 Dynamic DNS server IP/host to register IP addresses to.
+EOF
+}
+
+variable "tectonic_ddns_key_name" {
+  type    = "string"
+  default = ""
+
+  description = <<EOF
+(optional) This only applies if you use the modules/dns/ddns module.
+
+Specifies the RFC2136 Dynamic DNS server key name.
+EOF
+}
+
+variable "tectonic_ddns_key_algorithm" {
+  type    = "string"
+  default = ""
+
+  description = <<EOF
+(optional) This only applies if you use the modules/dns/ddns module.
+
+Specifies the RFC2136 Dynamic DNS server key algorithm.
+EOF
+}
+
+variable "tectonic_ddns_key_secret" {
+  type    = "string"
+  default = ""
+
+  description = <<EOF
+(optional) This only applies if you use the modules/dns/ddns module.
+
+Specifies the RFC2136 Dynamic DNS server key secret.
+EOF
 }

coreos/tectonic-installer

commit 8e16723cc440a33c9dbbada6b8936e31f58af267

config.tf: re-bump kubernetes/tectonic version to 1.6.7
This is a regression introduced in https://github.com/coreos/tectonic-installer/pull/1154.

config.tf
@@ -73,8 +73,8 @@ variable "tectonic_versions" {
     prometheus      = "v1.7.1"
     alertmanager    = "v0.7.1"
     monitoring      = "1.3.0"
-    kubernetes      = "1.6.6+tectonic.1"
-    tectonic        = "1.6.6-tectonic.1"
+    kubernetes      = "1.6.7+tectonic.1"
+    tectonic        = "1.6.7-tectonic.1"
     tectonic-etcd   = "0.0.1"
   }
 }