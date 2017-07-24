8597edeb0b6227a0933b898a5db8eae94125d8f0
cpanato <ctadeu@gmail.com>
2017-07-24 10:55:35 +0200

    ci/tests: Add readme and remane the jenkins job

--- tests/jenkins-jobs/tectonic_builder_docker_image.groovy
+++ tests/jenkins-jobs/tectonic_builder_docker_image.groovy
@@ -1,16 +1,16 @@
-#!groovy
+#!/bin/env groovy​
 
 folder("builders")
 
-job("builders/builder-tectonic-builder-upstream-terraform") {
+job("builders/tectonic-builder-docker-image") {
   logRotator(-1, 10)
-  description('Build a custom docker image of the tectonic builder using the upstream terraform. Changes here will be reverted automaticaly')
+  description('Build quay.io/coreos/tectonic-builder Docker image. Changes here will be reverted automaticaly.')
 
   label 'worker&&ec2'
 
   parameters {
-    stringParam('TERRAFORM_UPSTREAM_URL', '', 'upstream terraform download url')
-    stringParam('TECTONIC_BUILDER_VERSION', '', 'TECTONIC BUILDER Docker version')
+    stringParam('TERRAFORM_UPSTREAM_URL', '', 'upstream Terraform download url, defaults to CoreOS custom Terraform release (github.com/coreos/terraform)')
+    stringParam('TECTONIC_BUILDER_TAG', '', 'Tectonic Builder Docker tag')
     booleanParam('DRY_RUN', true, 'Just build the docker image')
   }
 
@@ -38,16 +38,16 @@ job("builders/builder-tectonic-builder-upstream-terraform") {
 
     if [ -z "\${TERRAFORM_UPSTREAM_URL}" ]
     then
-      export TECTONIC_BUILDER_NAME=quay.io/coreos/tectonic-builder:\${TECTONIC_BUILDER_VERSION}
-      docker build -t \${TECTONIC_BUILDER_NAME} -f images/tectonic-builder/Dockerfile .
+      export TECTONIC_BUILDER_IMAGE=quay.io/coreos/tectonic-builder:\${TECTONIC_BUILDER_TAG}
+      docker build -t \${TECTONIC_BUILDER_IMAGE} -f images/tectonic-builder/Dockerfile .
     else
-      export TECTONIC_BUILDER_NAME=quay.io/coreos/tectonic-builder:\${TECTONIC_BUILDER_VERSION}-upstream-terraform
-      docker build -t \${TECTONIC_BUILDER_NAME} --build-arg TERRAFORM_URL=\${TERRAFORM_UPSTREAM_URL} -f images/tectonic-builder/Dockerfile .
+      export TECTONIC_BUILDER_IMAGE=quay.io/coreos/tectonic-builder:\${TECTONIC_BUILDER_TAG}-upstream-terraform
+      docker build -t \${TECTONIC_BUILDER_IMAGE} --build-arg TERRAFORM_URL=\${TERRAFORM_UPSTREAM_URL} -f images/tectonic-builder/Dockerfile .
     fi
 
     if [ !\${DRY_RUN} = true  ] ; then
       docker login quay.io -u \${QUAY_USERNAME} -p \${QUAY_PASSWD}
-      docker push \${TECTONIC_BUILDER_NAME}
+      docker push \${TECTONIC_BUILDER_IMAGE}
     fi
   """.stripIndent()
     shell(cmd)
