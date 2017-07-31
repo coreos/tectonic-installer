8dbd2599eb7a4af7f2d52b39691603d7f9ecde7d
Max Leonard Inden <IndenML@gmail.com>
2017-07-31 08:19:55 +0200

    tests: Move credentials check to helper method

--- tests/rspec/lib/aws.rb
+++ tests/rspec/lib/aws.rb
@@ -3,11 +3,13 @@ require 'env_var'
 # AWS contains helper functions for the AWS cloud provider
 module AWS
   def self.check_prerequisites
-    credential_names = %w[AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY]
-    raise 'AWS credentials are not defined.' unless
-      EnvVar.set?(credential_names)
-
+    raise 'AWS credentials not defined' unless credentials_defined?
     raise 'TF_VAR_tectonic_aws_ssh_key is not defined' unless
-      EnvVar.set?(['TF_VAR_tectonic_aws_ssh_key'])
+      EnvVar.set?(%w[TF_VAR_tectonic_aws_ssh_key])
+  end
+
+  def self.credentials_defined?
+    credential_names = %w[AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY]
+    EnvVar.set?(credential_names)
   end
 end
