d7ef0468a52b8e692cdc2994d5ce35ecdbe5ae8b
Max Leonard Inden <IndenML@gmail.com>
2017-07-31 10:18:38 +0200

    tests: Infer cluster name prefix from tfvars file name

--- tests/rspec/lib/shared_examples/k8s.rb
+++ tests/rspec/lib/shared_examples/k8s.rb
@@ -3,8 +3,11 @@ require 'smoke_test'
 RSpec.shared_examples 'withCluster' do |tf_vars_path|
   before(:all) do
     AWS.check_prerequisites
-    # TODO: Infer cluster name from tfvars file name
-    @cluster = Cluster.new('aws', tf_vars_path)
+
+    prefix = tf_vars_path.scan(%r{\/(.*).tfvars.json$}).first.first
+    raise 'could not extract prefix from tfvars file name' if prefix == ''
+
+    @cluster = Cluster.new(prefix, tf_vars_path)
     @cluster.start
   end
 
