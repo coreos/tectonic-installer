8dca7b01a1c5b9583cf8d955288e9e8be2dc6333
Max Leonard Inden <IndenML@gmail.com>
2017-07-31 10:58:38 +0200

    tests: Refactor tfvars_file spec

--- tests/rspec/spec/tfvars_file_spec.rb
+++ tests/rspec/spec/tfvars_file_spec.rb
@@ -2,28 +2,29 @@ require 'tfvars_file'
 
 TFVARS_FILE_PATH = '../smoke/aws/vars/aws.tfvars.json'.freeze
 
-RSpec.describe 'tfvars_file' do
-  before(:all) do
-    @tfvars_file = TFVarsFile.new(TFVARS_FILE_PATH)
+describe TFVarsFile do
+  subject { described_class.new(TFVARS_FILE_PATH) }
+
+  context 'with wrong file path' do
+    subject { described_class.new('wrong-file-path') }
+    it 'throws an exception' do
+      expect { subject }.to raise_error(RuntimeError)
+    end
   end
 
-  it '.new raises exception with wrong file path' do
-    expect { TFVarsFile.new('wrong-file-path') }.to raise_error(RuntimeError)
+  it '#path returns the file path' do
+    expect(subject.path).to be(TFVARS_FILE_PATH)
   end
 
-  it '.path returns the file path' do
-    expect(@tfvars_file.path).to be(TFVARS_FILE_PATH)
+  it '#node_count returns correct #' do
+    expect(subject.node_count).to eq(7)
   end
 
-  it '.node_count returns correct #' do
-    expect(@tfvars_file.node_count).to eq(7)
+  it '#experimental? returns false if not set' do
+    expect(subject.experimental?).to eq(false)
   end
 
-  it '.experimental? returns false if not set' do
-    expect(@tfvars_file.experimental?).to eq(false)
-  end
-
-  it '.calico? returns false if not set' do
-    expect(@tfvars_file.experimental?).to eq(false)
+  it '#calico? returns false if not set' do
+    expect(subject.experimental?).to eq(false)
   end
 end
