5bea5f7ebbb2c8bf722dfa573a67313ed06595f3
Stephen Augustus <augustus@coreos.com>
2017-08-12 01:44:57 -0400

    azure: Remove hardcoded CL ver (`versions["container_linux"]`)

--- config.tf
+++ config.tf
@@ -70,14 +70,13 @@ variable "tectonic_versions" {
   type        = "map"
 
   default = {
-    container_linux = "1353.8.0"
-    etcd            = "3.1.8"
-    prometheus      = "v1.7.1"
-    alertmanager    = "v0.7.1"
-    monitoring      = "1.4.1"
-    kubernetes      = "1.7.1+tectonic.1"
-    tectonic        = "1.7.1-tectonic.2"
-    tectonic-etcd   = "0.0.1"
+    etcd          = "3.1.8"
+    prometheus    = "v1.7.1"
+    alertmanager  = "v0.7.1"
+    monitoring    = "1.4.1"
+    kubernetes    = "1.7.1+tectonic.1"
+    tectonic      = "1.7.1-tectonic.2"
+    tectonic-etcd = "0.0.1"
   }
 }
 
