623ebfbaa22ea80f292f166d5912259e8f4feddf
Sergiusz Urbaniak <sur@coreos.com>
2017-08-28 10:19:51 +0200

    modules/azure: remove stale azure_udev_rules definition

--- modules/azure/worker-as/ignition-worker.tf
+++ modules/azure/worker-as/ignition-worker.tf
@@ -29,16 +29,6 @@ data "ignition_file" "kubeconfig" {
   }
 }
 
-data "ignition_file" "azure_udev_rules" {
-  filesystem = "root"
-  path       = "/etc/udev/rules.d/66-azure-storage.rules"
-  mode       = 0644
-
-  content {
-    content = "${file("${path.module}/resources/66-azure-storage.rules")}"
-  }
-}
-
 data "ignition_file" "cloud-provider-config" {
   filesystem = "root"
   path       = "/etc/kubernetes/cloud/config"
