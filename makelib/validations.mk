b15925b55b9d51b964b43cc88baf48d286f1e0f6
Alex Somesan <alex.somesan@gmail.com>
2017-09-07 15:01:55 +0200

    Makefile: separate validation targets into own library

---
+++ makelib/validations.mk
@@ -0,0 +1,11 @@
+# This target is used by the GitHub PR checker to validate canonical syntax on all files.
+#
+.PHONY: structure-check
+structure-check:
+	$(eval FMT_ERR := $(shell terraform fmt -list -write=false .))
+	@if [ "$(FMT_ERR)" != "" ]; then echo "misformatted files (run 'terraform fmt .' to fix):" $(FMT_ERR); exit 1; fi
+
+	@if $(MAKE) docs && ! git diff --exit-code; then echo "outdated docs (run 'make docs' to fix)"; exit 1; fi
+	@if $(MAKE) examples && ! git diff --exit-code; then echo "outdated examples (run 'make examples' to fix)"; exit 1; fi
+	@if $(TOP_DIR)/modules/update-payload/make-update-payload.sh && ! git diff --exit-code; then echo "outdated payload (run '$(TOP_DIR)/modules/update-payload/make-update-payload.sh' to fix)"; exit 1; fi
+
