9d11d4d9a54529ab8247d39995b741e0b54ffc02
cpanato <ctadeu@gmail.com>
2017-10-13 15:50:50 +0200

    tests: making my friend rubocop happy

--- tests/rspec/lib/metal_cluster.rb
+++ tests/rspec/lib/metal_cluster.rb
@@ -6,32 +6,28 @@ require 'jenkins'
 require 'env_var'
 require 'metal_support'
 
-# AWSCluster represents a k8s cluster on AWS cloud provider
+# BareMetalCluster represents a k8s cluster on AWS cloud provider
 class MetalCluster < Cluster
-  def initialize(tfvars_file)
-    super(tfvars_file)
-  end
-
   def env_variables
-    root_dir = Dir.pwd
     variables = super
     variables['PLATFORM'] = 'metal'
     variables
   end
 
+  def master_ip_addresses
+    master_ip_address = []
+    master_ip_address.push(tf_value('var.tectonic_metal_controller_domains[0]'))
+  end
+
   def master_ip_address
-    Dir.chdir(@build_path) do
-      `echo 'var.tectonic_metal_controller_domains[0]' | terraform console ../../platforms/metal`.chomp
-    end
+    master_ip_addresses[0]
   end
 
   def tectonic_console_url
-    Dir.chdir(@build_path) do
-      console_url = `echo var.tectonic_metal_ingress_domain | terraform console ../../platforms/metal`.chomp
-      if console_url.empty?
-        raise 'should get the console url to use in the UI tests.'
-      end
-      console_url
+    console_url = tf_value('var.tectonic_metal_ingress_domain')
+    if console_url.empty?
+      raise 'failed to get the console url to use in the UI tests.'
     end
+    console_url
   end
 end
--- tests/rspec/spec/metal_basic_spec.rb
+++ tests/rspec/spec/metal_basic_spec.rb
@@ -7,15 +7,11 @@ TEST_CLUSTER_CONFIG_FILE = '../smoke/bare-metal/vars/metal.tfvars.json'
 
 RSpec.describe 'bare-metal-standard' do
   before(:context) do |_context|
-    # Save environment, to restore it once the test it done
-    # (since we alter it further down)
-    @curent_env = ENV.clone
     @varfile = TFVarsFile.new(TEST_CLUSTER_CONFIG_FILE)
     ENV['CLUSTER'] ||= NameGenerator.generate(@varfile.prefix)
     MetalSupport.install_base_software
     MetalSupport.setup_bare(@varfile)
     MetalSupport.start_matchbox
-
   end
 
   after(:context) do |_context|
