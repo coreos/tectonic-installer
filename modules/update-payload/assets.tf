7d8c4287a91856e5613fecc6f464c6740c758b1c
Jake Moshenko <jake.moshenko@coreos.com>
2017-11-16 11:29:01 -0500

    operators: Install the tectonic-alm-operator under TVO management

--- modules/update-payload/assets.tf
+++ modules/update-payload/assets.tf
@@ -9,6 +9,7 @@ resource "template_dir" "payload_operators" {
     tectonic_prometheus_operator_image = "${var.tectonic_container_images["tectonic_prometheus_operator"]}"
     tectonic_etcd_operator_image       = "${var.tectonic_container_images["tectonic_etcd_operator"]}"
     tectonic_cluo_operator_image       = "${var.tectonic_container_images["tectonic_cluo_operator"]}"
+    tectonic_alm_operator_image        = "${var.tectonic_container_images["tectonic_alm_operator"]}"
     image_re                           = "${var.tectonic_image_re}"
   }
 }
@@ -23,5 +24,6 @@ resource "template_dir" "payload_appversions" {
     tectonic_version               = "${var.tectonic_versions["tectonic"]}"
     tectonic_etcd_operator_version = "${var.tectonic_versions["tectonic-etcd"]}"
     tectonic_cluo_operator_version = "${var.tectonic_versions["cluo"]}"
+    tectonic_alm_operator_version  = "${var.tectonic_versions["alm"]}"
   }
 }
