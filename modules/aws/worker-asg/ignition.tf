d6136eca713d6dbf9653be74a15dc518a60327c4
Sergiusz Urbaniak <sur@coreos.com>
2017-11-21 12:14:51 +0100

    */aws: use S3 for ignition provisioning
    We hit the limits of the AWS userdata limit (20k) constantly.
    
    This fixes it by provisioning a minimal ignition configuration only
    which points to a replacement ignition configuration hosted on S3.
    
    This also removes workarounds/hacks to keep the userdata size small,
    especially for provisioning TLS assets on etcd nodes.

--- modules/aws/worker-asg/ignition.tf
+++ modules/aws/worker-asg/ignition.tf
@@ -4,7 +4,7 @@ data "ignition_config" "main" {
     "${var.ign_installer_runtime_mappings_id}",
     "${var.ign_max_user_watches_id}",
     "${var.ign_s3_puller_id}",
-    "${data.ignition_file.ca_cert_pem.*.id}",
+    "${var.ign_ca_cert_id_list}",
   ]
 
   systemd = [
@@ -14,19 +14,4 @@ data "ignition_config" "main" {
     "${var.ign_locksmithd_service_id}",
     "${var.ign_update_ca_certificates_dropin_id}",
   ]
-}
-
-data "ignition_file" "ca_cert_pem" {
-  count = "${var.ign_ca_cert_list_count}"
-
-  filesystem = "root"
-  path       = "/etc/ssl/certs/ca_${count.index}.pem"
-  mode       = 0400
-  uid        = 0
-  gid        = 0
-
-  source {
-    source       = "${var.ign_ca_cert_s3_list[count.index]}"
-    verification = "sha512-${sha512(var.ign_ca_cert_pem_list[count.index])}"
-  }
 }
