0f9188e14eaef7ac3c2ccffaaea2f8c49cb93707
Sergiusz Urbaniak <sur@coreos.com>
2017-11-21 12:11:51 +0100

    modules/ignition: unify etcd certificate generation
    Currently etcd certificate ignition files are created in their
    respective platform module.
    
    This unifies it by declaring the ignition file units centrally in the
    ignition module.

--- modules/ignition/outputs.tf
+++ modules/ignition/outputs.tf
@@ -131,3 +131,15 @@ output "ca_cert_pem_list" {
     "${var.custom_ca_cert_pem_list}",
   ]
 }
+
+output "etcd_crt_id_list" {
+  value = [
+    "${data.ignition_file.etcd_ca.*.id}",
+    "${data.ignition_file.etcd_client_key.*.id}",
+    "${data.ignition_file.etcd_client_crt.*.id}",
+    "${data.ignition_file.etcd_server_key.*.id}",
+    "${data.ignition_file.etcd_server_crt.*.id}",
+    "${data.ignition_file.etcd_peer_key.*.id}",
+    "${data.ignition_file.etcd_peer_crt.*.id}",
+  ]
+}
