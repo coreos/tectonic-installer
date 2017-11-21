c8e828a4786274f2859d4a54a0c38eb3b994dd27
Sergiusz Urbaniak <sur@coreos.com>
2017-11-21 12:11:51 +0100

    modules/ignition: unify etcd certificate generation
    Currently etcd certificate ignition files are created in their
    respective platform module.
    
    This unifies it by declaring the ignition file units centrally in the
    ignition module.

--- modules/ignition/variables.tf
+++ modules/ignition/variables.tf
@@ -133,6 +133,30 @@ variable "etcd_ca_cert_pem" {
   description = "The etcd kube CA certificate in PEM format."
 }
 
+variable "etcd_client_key_pem" {
+  default = ""
+}
+
+variable "etcd_client_crt_pem" {
+  default = ""
+}
+
+variable "etcd_server_key_pem" {
+  default = ""
+}
+
+variable "etcd_server_crt_pem" {
+  default = ""
+}
+
+variable "etcd_peer_key_pem" {
+  default = ""
+}
+
+variable "etcd_peer_crt_pem" {
+  default = ""
+}
+
 variable "custom_ca_cert_pem_list" {
   type        = "list"
   description = "(optional) A list of custom CAs in PEM format."
