cabbc7402f9a1ca24ae0b5987af65c7ef830c9e4
Enxebre <alberto.garcial@hotmail.com>
2018-01-19 13:40:49 +0100

    Inject kubeconfig file from ignition

--- platforms/aws/main.tf
+++ platforms/aws/main.tf
@@ -171,6 +171,7 @@ module "masters" {
   s3_bucket                            = "${aws_s3_bucket.tectonic.bucket}"
   ssh_key                              = "${var.tectonic_aws_ssh_key}"
   subnet_ids                           = "${module.vpc.master_subnet_ids}"
+  kubeconfig_content                   = "${module.bootkube.kubeconfig}"
 }
 
 module "ignition_workers" {
@@ -229,6 +230,7 @@ module "workers" {
   subnet_ids                           = "${module.vpc.worker_subnet_ids}"
   vpc_id                               = "${module.vpc.vpc_id}"
   worker_iam_role                      = "${var.tectonic_aws_worker_iam_role_name}"
+  kubeconfig_content                   = "${module.bootkube.kubeconfig}"
 }
 
 module "dns" {
