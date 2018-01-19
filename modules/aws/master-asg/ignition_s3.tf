e3b5751c63ee7f5e48b203ffe8a1cdc4266131e8
Alberto <alberto.garcial@hotmail.com>
2018-01-19 12:01:09 +0100

    add initial aws support for bootstrapping through ncg (#2758)

--- modules/aws/master-asg/ignition_s3.tf
+++ modules/aws/master-asg/ignition_s3.tf
@@ -18,19 +18,7 @@ data "aws_region" "region" {
 }
 
 data "ignition_config" "s3" {
-  append {
+  replace {
     source = "http://${var.cluster_name}-ncg.${var.base_domain}/ignition?profile=controller"
-  }
-
-  files = ["${data.ignition_file.kubeconfig.id}"]
-}
-
-data "ignition_file" "kubeconfig" {
-  filesystem = "root"
-  path       = "/etc/kubernetes/kubeconfig"
-  mode       = 0644
-
-  content {
-    content = "${var.kubeconfig_content}"
   }
 }
