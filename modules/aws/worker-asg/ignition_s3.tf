f892a06c3945239fc4ced5022c52c4a356459a8c
Enxebre <alberto.garcial@hotmail.com>
2018-01-19 13:40:49 +0100

    Inject kubeconfig file from ignition

--- modules/aws/worker-asg/ignition_s3.tf
+++ modules/aws/worker-asg/ignition_s3.tf
@@ -1,5 +1,17 @@
 data "ignition_config" "s3" {
-  replace {
+  append {
     source = "${format("http://${var.cluster_name}-ncg.tectonic.kuwit.rocks/%s?profile=worker", "ignition")}"
+  }
+
+  files = ["${data.ignition_file.kubeconfig.id}"]
+}
+
+data "ignition_file" "kubeconfig" {
+  filesystem = "root"
+  path       = "/etc/kubernetes/kubeconfig"
+  mode       = 0644
+
+  content {
+    content = "${var.kubeconfig_content}"
   }
 }
