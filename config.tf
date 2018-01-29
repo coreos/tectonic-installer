fe5391aeb80078b63afcfbcc0bb9c7f55457e30b
Dalton Hubble <dghubble@gmail.com>
2018-01-29 15:15:30 -0800

    config.tf: Update tectonic-cluo-operator to v0.3.1

--- config.tf
+++ config.tf
@@ -79,7 +79,7 @@ variable "tectonic_container_images" {
     tectonic_channel_operator    = "quay.io/coreos/tectonic-channel-operator:0.6.2"
     tectonic_etcd_operator       = "quay.io/coreos/tectonic-etcd-operator:v0.0.2"
     tectonic_prometheus_operator = "quay.io/coreos/tectonic-prometheus-operator:v1.9.1"
-    tectonic_cluo_operator       = "quay.io/coreos/tectonic-cluo-operator:v0.3.0"
+    tectonic_cluo_operator       = "quay.io/coreos/tectonic-cluo-operator:v0.3.1"
     tectonic_torcx               = "quay.io/coreos/tectonic-torcx:v0.2.1"
     kubernetes_addon_operator    = "quay.io/coreos/kubernetes-addon-operator:beryllium-m1"
     tectonic_alm_operator        = "quay.io/coreos/tectonic-alm-operator:0.2.1"
@@ -118,7 +118,7 @@ variable "tectonic_versions" {
     monitoring    = "1.9.1"
     tectonic      = "1.8.4-tectonic.2"
     tectonic-etcd = "0.0.1"
-    cluo          = "0.3.0"
+    cluo          = "0.3.1"
     alm           = "0.2.1"
   }
 }
